d758bbc5396231ca431c725b83f194575de5be1b
Michał Dudek <michal@michaldudek.pl>
2014-12-15 22:02:07 +0100

    Adjust the dummy module.

--- src/Acme/Modules/Demo/AcmeDemoModule.php
+++ src/Acme/Modules/Demo/AcmeDemoModule.php
@@ -6,8 +6,24 @@ use Splot\Framework\Modules\AbstractModule;
 class AcmeDemoModule extends AbstractModule
 {
 
-    public function boot() {
-        
+    /**
+     * This method is called on the module during configuration phase so you can register any services,
+     * listeners etc here.
+     *
+     * It should not contain any logic, just wiring things together.
+     *
+     * If the module contains any routes they should be registered here.
+     */
+    public function configure() {
+        parent::configure();
+    }
+
+    /**
+     * This method is called on the module during the run phase. If you need you can include any logic
+     * here.
+     */
+    public function run() {
+        parent::run();
     }
 
 }
